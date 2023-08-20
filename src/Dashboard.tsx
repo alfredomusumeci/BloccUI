45b3443ec22a8a6b7e7fb431df78942470377f31
Alfredo Musumeci <alfredo.msmeci@gmail.com>
2023-08-20 11:08:21 +0100

    added date & time filtering

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { Container, Table, TableHead, TableRow, TableCell, TableBody, TablePagination, Box } from '@mui/material';
+import {
+  Container, Table, TableHead, TableRow, TableCell, TableBody, TablePagination, Box,
+  TextField, Grid, Button
+} from '@mui/material';
 
 function Dashboard() {
   // Pagination
@@ -7,6 +10,14 @@ function Dashboard() {
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
   // Filters
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  // Reset filters
+  const resetDates = () => {
+    setStartDate('');
+    setEndDate('');
+  };
 
   // Mock data
   const sensingData = [
@@ -15,39 +26,87 @@ function Dashboard() {
     // ... more sensing data
   ];
 
+  const handleFilter = () => {
+    if (!startDate && !endDate) {
+      return sensingData; // Return all data if no dates are set
+    }
+
+    let startTimestamp = startDate ? new Date(startDate).getTime() : Number.MIN_SAFE_INTEGER;
+    let endTimestamp = endDate ? new Date(endDate).getTime() : Number.MAX_SAFE_INTEGER;
+
+    if (startDate && !startDate.includes('T')) {
+      startTimestamp = new Date(startDate).setHours(0, 0, 0, 0);
+      endTimestamp = new Date(startDate).setHours(23, 59, 59, 999);
+    }
+
+    return sensingData.filter(data => data.timestamp >= startTimestamp && data.timestamp <= endTimestamp);
+  };
+
+  const filteredData = handleFilter();
+
   return (
-      <Container>
-        <Box bgcolor="white" boxShadow={3} p={3} borderRadius={2} my={4}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Timestamp</TableCell>
-                <TableCell>Temperature</TableCell>
-                <TableCell>Approvals</TableCell>
-                <TableCell>Transaction ID</TableCell>
+<    Container>
+      <Box bgcolor="white" boxShadow={3} p={3} borderRadius={2} my={4}>
+        <Grid container spacing={3}>
+          <Grid item xs={5}>
+            <TextField
+              label="Start Date & Time"
+              type="datetime-local"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <TextField
+              label="End Date & Time"
+              type="datetime-local"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              fullWidth
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={2} container alignItems="center">
+            <Button 
+              variant="outlined" 
+              onClick={resetDates} 
+              style={{ height: '100%', color: 'red', borderColor: 'red' }}>
+              Reset
+            </Button>
+          </Grid>
+        </Grid>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Timestamp</TableCell>
+              <TableCell>Temperature</TableCell>
+              <TableCell>Approvals</TableCell>
+              <TableCell>Transaction ID</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((data) => (
+              <TableRow key={data.txId}>
+                <TableCell>{new Date(data.timestamp).toLocaleString()}</TableCell>
+                <TableCell>{data.temperature}°C</TableCell>
+                <TableCell>{data.approvals}</TableCell>
+                <TableCell>{data.txId}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {sensingData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((data) => (
-                <TableRow key={data.txId}>
-                  <TableCell>{new Date(data.timestamp).toLocaleString()}</TableCell>
-                  <TableCell>{data.temperature}°C</TableCell>
-                  <TableCell>{data.approvals}</TableCell>
-                  <TableCell>{data.txId}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Box>
-        <TablePagination
-          component="div"
-          count={sensingData.length}
-          page={page}
-          onPageChange={(event, newPage) => setPage(newPage)}
-          rowsPerPage={rowsPerPage}
-          onRowsPerPageChange={(event) => setRowsPerPage(parseInt(event.target.value, 10))}
-        />
-      </Container>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+      <TablePagination
+        component="div"
+        count={filteredData.length}
+        page={page}
+        onPageChange={(event, newPage) => setPage(newPage)}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={(event) => setRowsPerPage(parseInt(event.target.value, 10))}
+      />
+    </Container>
   );
 }
 
